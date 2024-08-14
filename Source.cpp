a1e4a2c2c8e7c2ae52f344359eedfd6d824224ad
Amalya <amalya.fatullaeva97@gmail.com>
2024-08-15 00:36:43 +0700

    Добавлена возможность записи найденных слов в массив для дальнейшего извлечения

--- Source.cpp
+++ Source.cpp
@@ -20,23 +20,50 @@ int main()
 
 	char c = 'y';
 	string key;
-	string result;
-	int i = 0;
+	
+	
+	
+	
 	while (c == 'y')
 	{
+		int j = 0;
+		int count = 0;
+		string result{};
+		string word{};
+		string* res = new string[n]{};
 		cout << "\nВведите часть слова:" << endl;
 		cin >> key;
 		autocomplete(root, key,result);
-		int f = sizeof(result) / sizeof(result[0]);
-		if (f > 1)
+		int f = result.size();
+
+		for (int i = 0; i < f; i++)
+		{
+
+			if (result[i] != 32)
+			{
+				word += result[i];
+			}
+
+			else
+			{
+				res[j++] = word;
+				count++;
+				word = {};
+			}
+		}
+
+		if (count > 1)
 		{
 			cout << "\nВ словаре есть следующие слова с введённым префиксом:" << endl;
-			cout << result << endl;
+			for (int j = 0; j < count; j++)
+			{
+				cout << res[j] << endl;
+			}
 		}
-		if (f == 1)
+		if (count == 1)
 		{
 			cout << "\nВ словаре есть одно слово с введённым префиксом:" << endl;
-			cout << result << endl;
+			cout << res[0] << endl;
 		}
 		cout << "\nХотите продолжить ввод?(y/n)" << endl;
 		cin >> c;
